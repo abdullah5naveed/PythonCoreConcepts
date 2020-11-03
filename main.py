2726174a210fc598333d09aa0b986cbfa35fdf10
abdullah5naveed <abdullah5naveed@gmail.com>
2020-11-03 15:02:53 +0500

    First Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# name = "john Smith"
+# age = 20
+# is_new = True
+# print(name, age, is_new)
+
+# name = input("What is your name? ")
+# color = input("Whats your favrt color? ")
+# print(name + " likes " + color)
+
+# weight = input("Enter your Weight in Kilo-grams: ")
+# print(type(weight))
+# weight_pounds = float(weight) * 2.205
+# print(type(weight_pounds))
+# print("Your weight in pounds is: ", weight_pounds)
+
+    ##logical Operators
+# price = 1000000
+# is_good_credit = True
+# if is_good_credit:
+#     n_price = 0.1 * price
+# else:
+#     n_price = 0.2 * price
+# print(f"Down payment is: ${n_price}")
+
+    ##Comparison Operatores
+# name = input("Enter Your Name: ")
+# if len(name) < 3:
+#     print("Name is too short.")
+# elif len(name) > 50:
+#     print("Name is too long")
+# else:
+#     print("name looks good...")
+
